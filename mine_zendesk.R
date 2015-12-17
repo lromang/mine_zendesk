782fc9d8c685efba5883891492742e3c788cd865
lromang <luis.roangarci@gmail.com>
2015-12-17 10:47:56 -0600

    code

--- mine_zendesk.R
+++ mine_zendesk.R
@@ -1,67 +1,108 @@
-library(RJSONIO)
-library(plyr)
-library(RCurl)
+#! /usr/bin/Rscript
+
 library(dplyr)
+library(plyr)
+library(stringr)
+library(tidyr)
 
-## Lectura de datos
-users         <- RJSONIO::fromJSON("users.json")
-organizations <- RJSONIO::fromJSON("organizations.json")
-satisfaction_ratings <- RJSONIO::fromJSON("satisfaction_ratings.json")
+##############################
+## Read in data
+##############################
+usr <- readLines("users.txt")
+sat <- readLines("satisfaction_ratings.txt")
+org <- readLines("organizations.txt")
 
-## Minar satisfaccion
-requester   <- laply(satisfaction_ratings, function(t) t <- t$requester)
-assignee    <- laply(satisfaction_ratings, function(t) t <- t$requester)
-score       <- laply(satisfaction_ratings, function(t) t <- t$score)
-comment     <- laply(satisfaction_ratings, function(t) t <- {
-    if(is.null(t$comment)){
-        NA
-    }else{
-        t$comment
-    }
-})
-create_date <- laply(satisfaction_ratings, function(t) t <- t$created_at)
+##############################
+## structure user
+##############################
+## requester
+requester <- usr[str_detect(usr, "requester")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
 
-## dataset
-satis_data  <-
-    data.frame(requester = requester,
-               score = score,
-               comment = comment,
-               create_date = create_date)
+## name
+name <- usr[str_detect(usr, "u_name:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
 
-## Minar usuarios
-requester    <- laply(users, function(t) t <- t$id)
-url          <- laply(users, function(t) t <- t$url)
-u_name       <- laply(users, function(t) t <- t$name)
-email        <- laply(users, function(t) t <- t$email)
-role         <- laply(users, function(t) t <- t$role)
-organization <- laply(users, function(t) t <- {
-    if(is.null(t$organization_id)){
-        NA
-    }else{
-        t$organization_id
-    }
-})
+## role
+role <- usr[str_detect(usr, "role:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
 
-## dataset
-users_data   <- data.frame(requester = requester,
-                          name = u_name, email = email,
-                          role = role, organization = organization)
-users_data   <- dplyr::filter(users_data, role == "end-user")
+## org_usr
+org_usr <- usr[str_detect(usr, "org:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
 
-## Minar organizaciones
-id    <- laply(organizations, function(t) t <- t$id)
-org_name <- laply(organizations, function(t) t <- t$details)
+## email
+email <- usr[str_detect(usr, "email:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
 
-## dataset
-org_data <- data.frame(organization = id, org = org_name)
+## datauser
+data_usr <- data.frame(requester = requester,
+                       name = name,
+                       role = role,
+                       id_org = org_usr,
+                       email = email)
 
-## dataset
-satis_u <- merge(satis_data, users_data, by = "requester",    all = TRUE)
-org_u   <- merge(satis_u,      org_data, by = "organization", all = TRUE)
+##############################
+## structure satisfaction
+##############################
+## requester
+sat_requester <- sat[str_detect(sat, "requester_id:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
+
+## score
+score <- sat[str_detect(sat, "score:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
+
+## score
+comment <- sat[str_detect(sat, "comment:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
+
+## date
+date <- sat[str_detect(sat, "date:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim() %>% as.Date()
+
+data_sat <- data.frame(requester = sat_requester,
+                      score = score,
+                      comment = comment,
+                      date = date)
+
+##############################
+## structure org
+##############################
+## id_org
+id_org <- org[str_detect(org, "id:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
+
+## org_name
+org_name <- org[str_detect(org, "org_name:")] %>%
+    lapply(function(t)t <- str_split(t, ":")[[1]][2]) %>%
+    unlist() %>% str_replace(",","") %>% str_trim()
+
+data_org <- data.frame(id_org = id_org,
+                      org_name = org_name)
+
+##############################
+## putting all together
+##############################
+## satisfaction usr
+sat_usr <- merge(data_sat, data_usr, by = "requester")
+
+## satisfaction usr org
+sat_usr_org <- merge(sat_usr, data_org, by = "id_org")
 
 ## final data
-final_data <- org_u[,3:ncol(satis_u)]
-final_data$role <- NULL
+final_data <- sat_usr_org[,-c(1,2)]
+final_data <- final_data[,c(4,5,6,7,1,2)]
 
-## Escribir resultados
-write.csv(final_data, "data_zen.csv", row.names = FALSE)
+## Write results
+write.csv(final_data, "zendesk.csv", row.names = FALSE)
